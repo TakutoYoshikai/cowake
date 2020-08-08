1157dfb71f8ff86f58bb5e5aa0e0df56b766dd7b
takuto <takuto.yoshikai@gmail.com>
2020-08-08 11:56:44 +0900

    offline

--- cowake-offline.js
+++ cowake-offline.js
@@ -15,6 +15,28 @@ function showRooms(json) {
   tableTag += "</table>";
   document.getElementById("result").insertAdjacentHTML("beforeend", tableTag);
 }
+
+function showTables(json) {
+  console.log(json);
+  let sessions = json.sessions;
+  for (let j = 0; j < sessions.length; j++) {
+    let session = sessions[j];
+    let tableTag = "セッション" + (j + 1);
+    tableTag += "<table>";
+    for (let i = 0; i < session.length; i++) {
+      let room = session[i];
+      tableTag += "<tr>";
+      tableTag += "<td>ルーム" + (i + 1) + "</td>";
+      for (let member of room) {
+        tableTag += "<td>" + member.name + "</td>";
+      }
+      tableTag += "</tr>";
+    }
+    tableTag += "</table><br><br>";
+    document.getElementById("result").insertAdjacentHTML("beforeend", tableTag);
+  }
+}
+
 document.getElementById("submit").onclick = function() {
   let tsv = document.getElementById("tsv").value;
   let sessionTime = parseInt(document.getElementById("sessionTime").value);
